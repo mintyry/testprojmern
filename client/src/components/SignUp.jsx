c87bfe85c5b943dbcac2da9b0d07ab3dbf884794
mintyry <gitmintyry@gmail.com>
2024-01-30 21:01:20 -0800

    set up page components

---
+++ client/src/components/SignUp.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { useMutation } from '@apollo/client';
+import { CREATE_USER } from "../utils/mutations";
+
+
+function SignUp() {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        username: '',
+        password: ''
+    });
+
+    const [createUser] = useMutation(CREATE_USER);
+
+    const handleChange = (event) => {
+        const  { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const { data } = await createUser({
+            ...formState
+        });
+
+        // save token to localStorage
+
+
+        console.log(data.token)
+    }
+
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>
+                        Name: <br/>
+                        <input type = "text" name = "name" value = {formState.name} onChange={handleChange} />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Username: <br/>
+                        <input type = "text" name = "username" value = {formState.username} onChange={handleChange} />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        password: <br/>
+                        <input type = "text" name = "password" value = {formState.password} onChange={handleChange} />
+                    </label>
+                </div>
+
+                <div>
+                    <button type="submit">Sign up</button>
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default SignUp;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './components/Home.jsx';
+import QRCode from './components/QRCode.jsx';
+import SignUp from './components/SignUp.jsx';
+
+const browserRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/qrcode/:qrCodeId',
+        element: <QRCode />
+      },
+      {
+        path: '/signup',
+        element: <SignUp />
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={browserRouter} />
   </React.StrictMode>,
 )
